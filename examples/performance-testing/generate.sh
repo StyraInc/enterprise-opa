ae37b3015474d58c3b2f42388356174543701a9d
Charlie Egan <charlieegan3@users.noreply.github.com>
2023-02-06 15:20:39 +0000

    Revise self gen instructions, drop jq

--- examples/performance-testing/generate.sh
+++ examples/performance-testing/generate.sh
@@ -2,12 +2,6 @@
 
 set -eo pipefail
 
-if command -v jq>/dev/null 2>&1 ; then
-    echo "jq version: $(jq --version)"
-else
-    echo "jq not found"
-    exit 1
-fi
 if command -v opa>/dev/null 2>&1 ; then
     echo "opa version: $(opa version | head -n 1 | cut -d ' ' -f 2)"
 else
@@ -19,16 +13,23 @@ echo "Generating test data with config:"
 
 cat generate-config.json
 
+bundle_path="$(PWD)/bundle"
+
+rm -rf "$bundle_path"
+mkdir -p "$bundle_path"
+mkdir -p "$bundle_path/users"
+mkdir -p "$bundle_path/roles"
+
 temp_dir=$(mktemp -d)
 data_file="$temp_dir"/data.json
 
-load eval -d generate.rego -i generate-config.json data.generate | jq .result[0].expressions[0].value > "$data_file"
+opa eval -d generate.rego -i generate-config.json data.generate > "$data_file"
 
-jq .queries "$data_file" > queries
-mkdir -p bundle/
-jq .users,.roles "$data_file" > bundle/data.json
+opa eval 'input.result[0].expressions[0].value.queries' -fraw -i "$data_file" > queries
+opa eval 'input.result[0].expressions[0].value.users' -fraw -i "$data_file" > "$bundle_path/users/data.json"
+opa eval 'input.result[0].expressions[0].value.roles' -fraw -i "$data_file" > "$bundle_path/roles/data.json"
 
-echo "New bundle created in ./bundle"
+echo "New bundle created at $bundle_path"
 du -sh bundle
 
 rm -r "$temp_dir"
